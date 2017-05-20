7dd4d496231d850eb3a0ddd017334000bce6b9de
sidheshwar <sidheshwar06@gmail.com>
2017-05-20 20:15:23 +0530

    first commit

---
+++ src/org/opengts/CompileTime.java
@@ -0,0 +1,18 @@
+package org.opengts;
+public class CompileTime
+{
+    // -- 2017/05/12 15:37:49 IST
+    public static final String  SERVICE_ACCOUNT_ID   = "opengts";
+    public static final String  SERVICE_ACCOUNT_NAME = "Open Source OpenGTS";
+    public static final String  SERVICE_ACCOUNT_HASH = "8E8AD5BD55CCBA73A679AC720DC08B8A";
+    public static final String  SERVICE_ACCOUNT_KEY  = "";
+    public static final long    COMPILE_TIMESTAMP    = 1494583669L;
+    public static final String  COMPILE_DATETIME     = "2017/05/12 15:37:49 IST";
+    public static final long    COMPILE_SEED         = 0x42B24966E033931EL;
+    // --
+    public static final String  getProperty(String key, String  dft) { return dft; }
+    public static final double  getProperty(String key, double  dft) { return dft; }
+    public static final long    getProperty(String key, long    dft) { return dft; }
+    public static final int     getProperty(String key, int     dft) { return dft; }
+    public static final boolean getProperty(String key, boolean dft) { return dft; }
+}
